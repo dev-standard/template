749a970483557c39f0d59b8d4c616597f1ef7edf
liting <luz.liting@gmail.com>
2022-12-18 15:56:31 +0800

    perf(eslint): add eslint as devDependencie

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,7 +2,7 @@
  * @Date: 2022-12-16 22:42:01
  * @Author: liting luz.liting@gmail.com
  * @LastEditors: liting luz.liting@gmail.com
- * @LastEditTime: 2022-12-16 23:04:01
- * @FilePath: /template/commitlint.config.js
+ * @LastEditTime: 2022-12-18 15:59:51
+ * @FilePath: /template/commitlint.config.cjs
  */
-module.exports = { 'extends': [ '@commitlint/config-conventional' ]}
+module.exports = { extends: ['@commitlint/config-conventional'] }
